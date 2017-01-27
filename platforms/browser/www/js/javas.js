c97793a746e5583bc14a7382476270c9419a976f
Daniel Roman <roman.martinez.daniel@gmail.com>
2017-01-27 22:23:57 +0100

    First Commit

---
+++ platforms/browser/www/js/javas.js
@@ -0,0 +1,18 @@
+function getNameAndPrint(){
+	var nombre = $('#nombre').val();
+	$('#nombre_introducido').show();
+	$('#nombre_introducido').html('Tu nombre es: <strong>'+nombre+'<strong>');
+}
+
+$(function() {
+
+	alert('THE APP STARTED!');
+
+	$('button').on("click", function() {
+		getNameAndPrint();		
+	});
+
+	$( "#form_nombre" ).submit(function( event ) {
+		getNameAndPrint();		
+	});
+});
